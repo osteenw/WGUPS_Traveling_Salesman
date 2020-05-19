c25139a79c971fa95aef1d66f9c541abafdaca9e
osteenw <58855696+osteenw@users.noreply.github.com>
2020-05-19 13:43:21 -0400

    Starting point

---
+++ Main.py
@@ -0,0 +1,5 @@
+from Graph import Vertex, Graph
+
+# Assign each location to a Vertex
+
+# Assigns each vertex to a graph
---
+++ Package.py
@@ -0,0 +1,10 @@
+class Package:
+    def __initi__(self, id, address, deadline, city, state, zip, weight, status):
+        self.id = id
+        self.address = address
+        self.deadline = deadline
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.weight = weight
+        self.status = status
