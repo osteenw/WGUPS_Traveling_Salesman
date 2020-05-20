5f8e67e60d43c5dbd3eb1aafab256ddc10edde7e
osteenw <58855696+osteenw@users.noreply.github.com>
2020-05-20 16:36:26 -0400

    Added graph

--- Graph.py
+++ Graph.py
@@ -2,8 +2,6 @@
 class Vertex:
     def __init__(self, label):
         self.label = label
-        self.distance = float('inf')
-        self.pred_vertex = None
 
 
 class Graph:
--- Main.py
+++ Main.py
@@ -21,20 +21,44 @@ with open('package_list.csv', encoding='utf-8-sig') as csv_file:
         hash_table.set(package)
         package_list.append(package)
 
+graph = Graph()
+address_matrix = []
 
+# Initializes a matrix for each address
+# Each address is set to a Vertex on the graph
 with open('address_matrix.csv', encoding='utf-8-sig') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    i = 1
+
+    # Create a list for each row in the file
+    # Runtime is O(n^2)
+    i = 0  # Iterable used for indexing the Vertex
     for row in csv_reader:
-        # Create package
-        column = row  # To look at columns within row
-        name = column[0]
-        address = column[1]
-        for j in range(1, len(package_list) + 1):
-            if hash_table.get(j).address == address:
-                print(f"Address ID: {i} | Package ID {hash_table.get(j).id} | {name}, {address}")
+        row_data = []
+
+        # add each column of data to the list
+        row_data.append(Vertex(i))
+        for column in row:
+            if column != '':
+                row_data.append(column)
+            else:
+                i += 1
+                break
+
+        # Adds the current row data to the list
+        address_matrix.append(row_data)
+
+        # Adds the current rows vertex to the graph
+        graph.add_vertex(row_data[0])
+
+# Adds edges between addresses on the graph
+# Runtime is O(n^2)
+address_index = 0
+for vertex in graph.adjacency_list:
+    i = 0
+    while i < len(address_matrix[address_index]) - 3:  # 3 is subtracted because we are skipping the first 3 columns
+        graph.add_undirected_edge(vertex,  # Current vertex / Same as vertex at address_matrix[address_index]
+                                  address_matrix[i][0],  # Vertex for weighted edge
+                                  address_matrix[address_index][i + 3])  # Weighted Edge
         i += 1
 
-
-
-# Assigns each vertex to a graph
+    address_index += 1
